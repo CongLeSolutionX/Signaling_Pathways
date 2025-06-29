b2f53845acd50eaf03087db9d8e6da76b35f4a9a
Cong Le <longchike@gmail.com>
2025-06-29 01:55:32 -0700

    update code

--- Signaling_Pathways/Signaling_PathwaysApp.swift
+++ Signaling_Pathways/Signaling_PathwaysApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Signaling_PathwaysApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignalingPathwaysView()
         }
     }
 }
