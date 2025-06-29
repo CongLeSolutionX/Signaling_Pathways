89c3ddcc482e2d67aab0d01452cb9bd215ba27a8
Cong Le <longchike@gmail.com>
2025-06-29 01:47:03 -0700

    The setup

---
+++ Signaling_Pathways/Signaling_PathwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Signaling_PathwaysApp.swift
+//  Signaling_Pathways
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Signaling_PathwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
